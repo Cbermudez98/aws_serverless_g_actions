d754ea0fcae32fa33e96dc4ce54f981e444188c6
Cesar Bermudez <54110212+Cbermudez98@users.noreply.github.com>
2023-11-08 22:49:36 -0500

    initial commit

---
+++ src/controller/randomNumber.js
@@ -0,0 +1,5 @@
+module.exports.generateNumber = () => {
+    const random = parseInt(Math.random() * 100);
+    console.log("🚀  ~ file: randomNumber.js:3 ~ random:", random);
+    return random;
+};
---
+++ src/handler.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const { generateNumber } = require("./controller/randomNumber");
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: generateNumber(),
+      },
+    ),
+  };
+};
