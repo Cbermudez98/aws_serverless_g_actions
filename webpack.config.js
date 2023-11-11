7d733da43fca0d54e69e8fdfff1e5feff6551b99
Cesar Bermudez <54110212+Cbermudez98@users.noreply.github.com>
2023-11-10 23:05:39 -0500

    Se ajusta workflow github

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-    entry: "./src/handler.js",
-    output: {
-        filename: "handler.js",
-        path: path.resolve(__dirname, "dist")
-    }
-};
